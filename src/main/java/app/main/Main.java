4edba3dd03fa52f77557f04938ee37ded2a86721
mostafa <mostafakhanderoo7@gmail.com>
2025-02-25 17:30:00 +0330

    initial commit

---
+++ src/main/java/app/main/Main.java
@@ -0,0 +1,21 @@
+package app.main;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            Banking System
+
+           1.add entity Customer Employee Account Credit_card  Bank_branch Boss Transaction BaseEntity
+ private String status; //  example "CREATED"، "PAID"، "PAYMENT_FAILED" or "CANCELLED"
+//todo design pattern
+//todo Stream
+//todo unit test
+//todo : dto  /  validation
+//todo : new dataBase
+//todo : docker <----
+//todo : optional in OneToMany
+//todo : jeneric
+
+         */
+    }
+}
