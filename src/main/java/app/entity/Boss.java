88e503e5e19189bc54dae38a2301e4a280595b45
mostafa <mostafakhanderoo7@gmail.com>
2025-03-03 00:21:14 +0330

    create Boss entity for Bank

---
+++ src/main/java/app/entity/Boss.java
@@ -0,0 +1,21 @@
+package app.entity;
+
+import app.base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Boss extends BaseEntity<Long> {
+    @Column(nullable = false)
+    private String firstname;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false ,unique = true)
+    private Long personnelCode;
+}
