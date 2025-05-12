2f1e97a00737863a99f9f3e2b06869c0af625adf
mostafa <mostafakhanderoo7@gmail.com>
2025-05-12 12:54:49 +0330

    refactor update customer

--- src/main/java/app/repository/CustomerRepository.java
+++ src/main/java/app/repository/CustomerRepository.java
@@ -1,26 +1,25 @@
 package app.repository;
 
 import app.base.repository.BaseRepository;
-import app.base.repository.BaseRepositoryImpl;
+import app.entity.Account;
+import app.entity.CreditCard;
 import app.entity.Customer;
 import org.hibernate.Session;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends BaseRepository<Long, Customer> {
-    @Override
-    Customer save(Session session, Customer customer);
 
-    @Override
-    Customer update(Session session, Customer customer);
+    Customer createAccountAndCustomer(Session session,Account account, Customer customer);
 
-    @Override
-    Optional<Customer> findById(Session session, Long id);
+    Optional<Customer> findByNationalCode(Session session ,String nationalCode);
 
-    @Override
-    List<Customer> findAll(Session session);
+    Long userNameCheck(Session session, String username);
 
-    @Override
-    void deleteById(Session session, Long id);
+    Long nationalCodeCheck(Session session ,String nationalCode);
+
+    Long creditCardCheck(Session session ,Customer customer, CreditCard creditCard);
+
+    void setCreditCardForCustomer(Session session, String nationalCode, CreditCard creditCard);
+
 }
