6eea8f81010413bba7942a0cf11eba3626298ec2
mostafa <mostafakhanderoo7@gmail.com>
2025-05-12 12:22:44 +0330

    refactor bank data user

--- src/main/java/app/entity/Account.java
+++ src/main/java/app/entity/Account.java
@@ -1,31 +1,28 @@
 package app.entity;
 
 import app.base.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
 
 @Entity
-@Setter
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Account extends BaseEntity<Long> {
-    @Column(nullable = false,unique = true)
-    private String Address;
+    @Column(nullable = false, unique = true)
+    private String address;
     @Column(nullable = false)
-    private  Double balance;
+    private Double balance;
+    @Column(nullable = false)
+    private boolean accountLock;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Bank bank;
 
-@OneToOne
-private Customer customer;
-@OneToOne
-private Bank bank;
-@OneToMany
-private List<Transaction> transaction;
 
 }
