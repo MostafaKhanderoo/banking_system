6e19da887c52209b102ab585243f4e3134330c4e
mostafa <mostafakhanderoo7@gmail.com>
2025-05-12 12:52:56 +0330

    debug repository customer to create customer account and credit

--- src/main/java/app/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/app/repository/impl/CustomerRepositoryImpl.java
@@ -1,46 +1,63 @@
 package app.repository.impl;
 
 import app.base.repository.BaseRepositoryImpl;
+import app.entity.Account;
+import app.entity.CreditCard;
 import app.entity.Customer;
 import app.repository.CustomerRepository;
 import org.hibernate.Session;
 
-import java.util.List;
 import java.util.Optional;
 
-public class CustomerRepositoryImpl extends BaseRepositoryImpl<Long, Customer> {
+public class CustomerRepositoryImpl extends BaseRepositoryImpl<Long, Customer>
+        implements CustomerRepository {
 
 
     public CustomerRepositoryImpl(Class<Customer> entity) {
         super(entity);
     }
 
-    @Override
-    public List<Customer> findAll(Session session) {
-        return super.findAll(session);
-    }
 
     @Override
-    public Customer save(Session session, Customer customer) {
-        session.persist(customer);
+    public Customer createAccountAndCustomer(Session session, Account account, Customer customer) {
+         session.persist(customer);
+        session.persist(account);
         return customer;
     }
 
     @Override
-    public Customer update(Session session, Customer customer) {
-        session.persist(customer);
-        return customer;
+    public Optional<Customer> findByNationalCode(Session session, String nationalCode) {
+        return   session.createQuery
+                        ("from Customer c where c.nationalCode = :nationalCode",Customer.class)
+                .setParameter("nationalCode",nationalCode).uniqueResultOptional();
     }
 
     @Override
-    public Optional<Customer> findById(Session session, Long id) {
-    return session.byId(Customer.class).loadOptional(id);
+    public Long userNameCheck(Session session, String username) {
+       return session.createQuery
+                ("SELECT count(id) from Customer u where u.username= :username ",Long.class)
+                       .setParameter("username",username).getSingleResult();
+
+    }
+
+    @Override
+    public Long nationalCodeCheck(Session session, String nationalCode) {
+        return session.createQuery
+                ("select count(id) from Customer n where n.nationalCode=:nationalCode",Long.class)
+                .setParameter("nationalCode",nationalCode).getSingleResult();
+    }
+
+    @Override
+    public Long creditCardCheck(Session session, Customer customer, CreditCard creditCard) {
+      return  session.createQuery("select  count(id) from Customer c where c.creditCard =:creditCard",Long.class)
+                .setParameter("creditCard",creditCard).getSingleResult();
     }
 
 
-
     @Override
-    public void deleteById(Session session, Long id) {
-        super.deleteById(session, id);
+    public void setCreditCardForCustomer(Session session, String nationalCode, CreditCard creditCard) {
+        session.createQuery("from  Customer c where c.nationalCode = :nationalCode ",Customer.class)
+                .setParameter("nationalCode" ,nationalCode).getSingleResult();
+
     }
 }
